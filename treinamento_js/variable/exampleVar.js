6d9e4c3bb464cf488dfae22379aa8830c21ed07e
iamrosada <luisrosada2018@gmail.com>
2021-08-07 15:51:21 +0300

    variable type var in javascript

---
+++ treinamento_js/variable/exampleVar.js
@@ -0,0 +1,29 @@
+/*Existem diferentes 
+tipos de dados que podemos 
+armazenar em variáveis. */
+var a;
+
+console.log(a); //undefined
+//declarando uma  var do type String
+
+var nome = "Rosada";
+console.log("the type string", nome); //Rosada
+//declarando uma  var do type Number
+var idade = 21;
+console.log("the type number", idade); //21
+
+//declarando uma var do Boolean
+var isTrue = true;
+console.log("the type boolean", isTrue); //true
+
+//declarando uma var do type Matriz
+
+var allColor = ["red", "yellow", "black"];
+console.log("the type array ", allColor);
+
+//declarando uma var do type object
+var pessoa = {
+  name: "Luis",
+  age: 21,
+};
+console.log("the type object", pessoa);
