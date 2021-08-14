2417e8f790184735771091928dbaf16a132fa26a
iamrosada <luisrosada2018@gmail.com>
2021-08-14 23:19:21 +0300

    added methode slice

---
+++ treinamento_js/types/slice.js
@@ -0,0 +1,11 @@
+/*
+ string.slice (start, [end]) - 
+ retorna uma substring da string com base
+ nos argumentos de índice “start” e “end”.
+*/
+
+const number = [1, 2, 3, 4, 5, 6];
+
+const number2 = number.slice(0, 5);
+
+console.log(number2);
