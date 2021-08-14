2084b8cc35aa33bce521da2162c1b940dab4121a
iamrosada <luisrosada2018@gmail.com>
2021-08-14 23:18:59 +0300

    added methode map

---
+++ treinamento_js/types/map.js
@@ -0,0 +1,11 @@
+const number = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const resultant = number.map(getSumma);
+
+let suma = 0;
+
+function getSumma(item, index, arr) {
+  return arr;
+}
+
+console.log(resultant);
