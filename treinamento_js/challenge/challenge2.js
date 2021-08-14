4427841b0ba6aa4e56f7c78c6ed9a85288ddb3c1
iamrosada <luisrosada2018@gmail.com>
2021-08-14 23:12:20 +0300

    new challeng is done

---
+++ treinamento_js/challenge/challenge2.js
@@ -0,0 +1,12 @@
+function getFibonacci() {
+  const num = 10;
+  var arr = [];
+  /*   var getSoma = []; */
+  for (var i = 0; i <= num; i++) {
+    /* getSoma[i] = arr[i + 1]; */
+
+    console.log(i);
+  }
+}
+
+getFibonacci();
